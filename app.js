e76d7c6d3a92bc1f76e5943bc5b99ac944603894
shaochenn <hnhngtb1111@gmail.com>
2023-05-01 02:11:22 +0800

    feat: add restaurant seeder

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const express = require('express')
-const app = express()
-const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose')
+const port = process.env.PORT
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+
+
 // setting static files
 app.use(express.static('public'))
+
+
+
+
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 
 app.get('/', (req, res) => {
   res.render('index', { restaurant: restaurantList.results })
