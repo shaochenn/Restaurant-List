faa087a3d246bdc7debb1b8f9b9d498345ff7bd3
shaochenn <hnhngtb1111@gmail.com>
2023-05-01 03:55:35 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,27 +2,22 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const mongoose = require('mongoose')
-const port = process.env.PORT
+const Restaurant = require('./models/restaurant')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const port = process.env.PORT
 const app = express()
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
-
-
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 // setting static files
 app.use(express.static('public'))
-
-
-
-
 
 const db = mongoose.connection
 // 連線異常
@@ -37,7 +32,10 @@ db.once('open', () => {
 
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', {restaurants}))
+    .catch(error => console.error(error))
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
