b8ee2e0e55297006f40e25ac44a1449bf5e4ebe5
shaochenn <hnhngtb1111@gmail.com>
2023-05-01 16:38:16 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -100,6 +100,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//delete restaurant
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
