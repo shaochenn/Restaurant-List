e04a6c715e4bbed8630e4c2e75cca9450aa9213a
shaochenn <hnhngtb1111@gmail.com>
2023-05-01 18:28:06 +0800

    migrate search route to home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,5 +9,20 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      const resultRestaurants = restaurants.filter( restaurant => {
+        const nameLowerCase = restaurant.name.toLowerCase()
+        const categoryLowerCase = restaurant.category.toLowerCase()
+        return nameLowerCase.includes(keyword.toLowerCase()) || categoryLowerCase.includes(keyword.toLowerCase()) 
+      })
+      res.render('index', {restaurants: resultRestaurants, keyword})
+    })
+    .catch(error => console.log(error))
+})
+
 // 匯出路由模組
 module.exports = router
